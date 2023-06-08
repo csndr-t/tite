1caa418d50b182dc16a27ef30a47d5d255baaeba
csndr-t <135983869+csndr-t@users.noreply.github.com>
2023-06-08 21:23:30 +0800

    Create streamlit_weh.py

---
+++ streamlit_weh.py
@@ -0,0 +1,20 @@
+import streamlit as st 
+
+st.set_page_config(page_title = 'i miss u :<', page_icon = ':growing_heart:', layout = 'wide')
+
+
+# -------------------- header ------------------------------
+with st.container():
+    st.subheader('surprise pi!')
+    st.title('hello, love :heart_decoration:')
+    st.write('i decided to code out this website for u hehe. hindi ako sanay na i dont give gifts sa monthsaries, \n but it\'s been that way for a few months na.')
+    st.write('this is like a pambawi for all the months na di ako nakapagbigay gift huhu. pYTHON LANG TO OK DONT JUDGE <//3')
+
+#--------------------- songs ---------------------------------
+with st.container():
+    st.subheader('\n\nthese are songs that remind me of u:')
+    st.write('[1. baliw - sud](https://open.spotify.com/track/7cHycXGo5HnVQ5NkI3dLWK?si=39a96a081b454caa)')
+    st.write('[2. nothing - bruno major](https://open.spotify.com/track/1lORkxEMmsCZqhoxcmk3A3?si=6d833d22a99d4d7e)')
+    st.write('[3. tahanan - adie](https://open.spotify.com/track/5CUQnKjA6nlteCnxMKsjIu?si=1827b5ad63044ca7)')
+    st.write('[4. kalawakan - juan karlos](https://open.spotify.com/track/6EYcB9BS4impPFgmFxMy7P?si=9154b1bedfdd43ee)')
+    st.write('[5. upuan - ben&ben](https://open.spotify.com/track/3k7SKhwbvE2lAisLx3jZWl?si=bcb6b9367dc34316)')
